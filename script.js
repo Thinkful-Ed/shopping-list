360cf3a52f4c003a95d4cde2861d1b891476db53
Alex Peter <apyells@gmail.com>
2019-04-01 10:27:02 -0500

    updated notes in js file

---
+++ script.js
@@ -0,0 +1,14 @@
+//FORM:
+//Create code that stops form from being submitted. Also
+//should code so that it adds a child list to the li items
+$("form").submit(function( event ) {
+    event.preventDefault();
+}
+
+
+//SHOPPING-ITEM-TOGGLE:
+//create functionality that, when pressed, crosses or
+//uncrosses the selected item from the list
+
+//SHOPPING-ITEM-DELETE:
+//create functionality that deletes this
