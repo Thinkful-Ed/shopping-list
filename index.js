1abd59e5291838dd9cb29081acf0faa15d2dc855
Matt Thornberry <mtberry16@hotmail.com>
2019-07-03 11:34:48 -0600

    Added functionality to shopping-item-toggle button to toggle css strikethrough on item name.

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ $(document).ready(() => {
   const $list = [];
   $('li').hide(0);
   $(':submit').on('click', () => {
-  event.preventDefault();
-  const $item = $('#shopping-list-entry').val();
-  if ($list.includes($item)) {
-    alert('That item is already on your list!')
-  } else if($item === '') {
-    alert('Please enter a valid list item.')
-  } else {
-    $list.push($item)
-    //The toggleClass needs to be fixed. Instead of adding the new items, it adds the new item plus the previous one. Not sure what is going on here, but I'll keep look for a fix.
-    $("li:first-child").clone().toggleClass($item).prependTo(".shopping-list").show();
-  $(`.${$item}>.shopping-item`).text($item);
-  }
-  
-  
+    event.preventDefault();
+    const $item = $('#shopping-list-entry').val();
+    let $date = new Date();
+    if ($list.includes($item)) {
+      alert('That item is already on your list!')
+    } else if($item === '') {
+      alert('Please enter a valid list item.')
+    } else {
+      $list.push($item)
+      //The toggleClass needs to be fixed. Instead of adding the new items, it adds the new item plus the previous one. Not sure what is going on here, but I'll keep looking for a fix.
+      $("li:first-child").clone().toggleClass($item).prependTo(".shopping-list").show();
+    $(`.${$item}>.shopping-item`).text(`${$item} added to list: ${$date}`);
+    }
+  });
+  // really struggled here. DOM was deaf until I used document, and passed in .shopping-item-toggle. Problem resolved. Learned to add an alert to test if button fucntionality was working at all. Really helped resolve problem.
+  $(document).on('click', '.shopping-item-toggle', () => {
+    $('.shopping-item').toggleClass('shopping-item__checked');
+  })
   console.log($list);
 });
-});
+
 
