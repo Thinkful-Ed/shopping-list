a31da5c3b0172b61ed6ba2f0534823dbc101f892
Joalvaca <52941546+Joalvaca@users.noreply.github.com>
2019-07-29 22:14:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $('#js-shopping-list-form').submit(function(event){
   
   event.preventDefault();
 
-  const listItem =$ ('js-shopping-list-entry').val();
+  const listItem = $('.js-shopping-list-entry').val();
 
   $('.js-shopping-list-entry').val('');
 
