35bda13c7e7d3a88daf5605b00041a2268139102
Kate Engstrom <katemacnd@gmail.com>
2019-10-01 15:31:01 -0500

    linked jquery and js files, started selector for button

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function clickSubmit() {
+        let submitButton = $('button').first();
+        submitButton.on('click', function(event) {
+            event.preventDefault()
+            console.log('clicked!')
+        });
+    };
+    
+    $(clickSubmit);
