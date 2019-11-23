4bf28aa4504ff3b8dca9ff69e7e01d9bb19b2d9c
Cameron DeCoster <cameron.decoster@gmail.com>
2019-11-23 07:37:41 -0700

    added delete function, add new items function; fixed event delegation for check and delete

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 function listClick() {
-    $('.shopping-item-toggle').on('click', function (event) {
-        console.log('clicked check button');
-        let item = $(this).closest('li').find('.shopping-item');
-        console.log(item);
-        $(item).toggleClass('shopping-item__checked');
-        //console.log($(this).closest('li').find('.shopping-item').css('text-decoration'));
+    // Check/uncheck item when check button is clicked -> use event delegation
+    $('.shopping-list').on('click', '.shopping-item-toggle', function (event) {
+        let item = $(this).closest('li').find('.shopping-item');        
+        $(item).toggleClass('shopping-item__checked');        
     });
 
-    $('.shopping-item-delete').on('click', function (event) {
-        console.log('clicked delete button');
+    // Delete item when delete button is clicked -> use event delegation
+    $('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+        let item = $(this).closest('li');
+        $(item).remove();
+    });
+
+    // Add item to list when input value is not empty
+    $('#js-shopping-list-form').children('button').on('click', function (event) {
+        // Stop form from submitting
+        event.preventDefault();
+
+        // Grab shopping list elements
+        let item = $(this).siblings('#shopping-list-entry');
+        let shoppingList = $(this).closest('.container').find('.shopping-list');
+
+        // Add item if input value is not empty
+        if ($(item).val()) {
+            $(shoppingList).append(`
+            <li>
+                <span class="shopping-item">${$(item).val()}</span>
+                <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+                </div>
+            </li>`);
+        }
     });
 }
 
