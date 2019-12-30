5a74aee94aee5dfc13aff3514677e79618bd6b8e
Nathaly Cuya <57621005+natcuya@users.noreply.github.com>
2019-12-30 01:11:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 $(function (){
- $('#js-shopping-list-form').submit(function(event){
+$('#js-shopping-list-form').submit(function(event){
  event.preventDefault();
-   const itemName = $('.shopping-list-entry').val();
-   $('.shopping-list-entry').val('');
- $('.shopping-list').append(
+ const itemName = $('.shopping-list-entry').val();
+$('.shopping-list-entry').val('');
+$('.shopping-list').append(
      `<li>
    <span class="shopping-item">${itemName}</span>
-   <div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span>
+   <div class="shopping-item-controls">
+   <button class="shopping-item-toggle"><span class="button-label">check</span>
    </button>
    <button class="shopping-item-delete">
    <span class="button-label">delete</span>
@@ -15,10 +16,9 @@ $(function (){
    </li>`);
   });
  
- $('.shopping-list').on('click','.shopping-item-delete', function(event){
+$('.shopping-list').on('click','.shopping-item-delete', function(event){
     $(this).closest('li').remove();
   });
- 
 $('.shopping-list').on('click','.shopping-item-toggle', function (event) {
  $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
 });
