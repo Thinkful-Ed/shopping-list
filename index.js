d32648f6552d412fe7b661b30376b19190f2ad26
jyip01 <64282396+jyip01@users.noreply.github.com>
2020-05-14 18:25:02 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+$(function(){
+  
+  $('#js-shopping-list-form').submit(function(event) {
+  
+    event.preventDefault();
+  
+    const listItem = $('.js-shopping-list-entry').val();
+  
+    $('.js-shopping-list-entry').val('');
+  
+   $('.shopping-list').append(
+      `<li>
+        <span class="shopping-item">${listItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+  });
+  
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+  
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
+});
