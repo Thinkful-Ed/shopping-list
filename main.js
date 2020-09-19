9a50794604c5a841b5e6846a7294a34bac41544b
thelmaijemma <68825395+thelmaijemma@users.noreply.github.com>
2020-09-19 13:13:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+$(function() {
+    // mechanism for adding new items to the shopping cart
+    $('#js-shopping-list-form').submit( event => {
+      // stop the default form submission behavior
+      event.preventDefault();
+      // in case there's already results
+  
+      const item = $("#shopping-list-entry").val();
+      // we'll store individual DOM elements 
+  
+      const newHtml = `<li><span class="shopping-item">` +item+ `</span><div class="shopping-item-controls"> <button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`;
+         
+      $(".shopping-list").append(newHtml);   
+      });
+      
+      //here is how we can "check" and uncheck shopping cart items 
+      $(".shopping-item-toggle").on('click', function(event) {
+        $(event.currentTarget).closest("li").toggleClass("shopping-item__checked");
+      });
+  
+      // and finally, this is how to permanently delete an item
+      $(".shopping-item-delete").on('click', function(event) {
+        $(event.currentTarget).closest("li").remove("li");
+      });
+  
+      })
+  
