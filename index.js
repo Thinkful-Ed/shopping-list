09ad1f35cd83844941e067ff87649242d91ab7fe
Joe Wickes <joe.c.wickes@gmail.com>
2020-10-19 16:30:40 -0400

    Add add item functionality

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 $(function() {
-
+  // Add List Item
+  $('#js-shopping-list-form').on('submit', function(e) {
+    e.preventDefault();
+    let newListItem = $('#shopping-list-entry').val();
+    $('.shopping-list').append(`<li>
+    <span class="shopping-item">${newListItem}</span>
+    <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+    <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+    <span class="button-label">delete</span>
+    </button>
+    </div>
+    </li>`);
+  });
 
   // Check
   $('.shopping-item-toggle').on('click', function(e) {
