bd1e10ec11165c3b76e46aafe08874093903748b
Joe Wickes <joe.c.wickes@gmail.com>
2020-10-19 16:21:01 -0400

    Add delete functionality

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ $(function() {
 
   // Check
   $('.shopping-item-toggle').on('click', function(e) {
-    let listItem = $(e.target).parent().parent().siblings('.shopping-item');
-    $(listItem).toggleClass('shopping-item__checked');
+    let listText = $(e.target).parent().parent().siblings('.shopping-item');
+    listText.toggleClass('shopping-item__checked');
   });
 
   // Delete
+  $('.shopping-item-delete').on('click', function(e) {
+    let parentLi = $(e.target).parent().parent().parent();
+    parentLi.remove();
+  });
 });
